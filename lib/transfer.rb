935339567bea894fd65278da7a24a0ff5c71faf4
Samuel T <ssamt1142@gmail.com>
2019-04-19 00:50:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Transfer
-  # your code here
+attr_accessor :status, :sender, :receiver, :amount
+attr_reader
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    if sender.BankAccount.valid? == true && receiver.BankAccount.valid? == true
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+  end
+
+  def reverse_transfer
+  end
 end
